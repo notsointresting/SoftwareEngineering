55705aee4f727cdd14e78b6a2ccdccbb58d6cd33
Sahil Rajesh Shriwardhankar <102847495+notsointresting@users.noreply.github.com>
2022-12-20 21:27:05 +0530

    SE

--- SoftwareEngineering/Lab Assesment/Medium.py
+++ SoftwareEngineering/Lab Assesment/Medium.py
@@ -2,17 +2,18 @@ from email.message import EmailMessage # It provides creating or modifying struc
 import imghdr  # The imghdr module determines the type of image 
 import os 
 from PIL import Image  #PIL is Python Image Library
-#import pywhatkit  # Ro Send Message on whatsapp, Pywhatkit library is used.
+import pywhatkit  # Ro Send Message on whatsapp, Pywhatkit library is used.
 import smtplib 
-import urllib.request # request module defines functions and classes which help in opening URLs
+import urllib.request# request module defines functions and classes which help in opening URLs
+import GetPass as G
 
 
 msg = EmailMessage()
-Sender_Mail = "hbd.service.01@gmail.com"
-password = "hfsuwibmkiqtdmvm"
+Sender_Mail = G.Mail
+password = G.PassWord
 
 
-def sendMail(Name,Reciver_Mail):
+def sendMail(Name,Reciver_Mail,Text):
     server = smtplib.SMTP('smtp.gmail.com',587) #Created gmail's server, and connected to gmail API
     # Adding transfer layered security
     server.starttls()
@@ -23,45 +24,11 @@ def sendMail(Name,Reciver_Mail):
     msg['From'] = Sender_Mail
     msg['To'] = Reciver_Mail
     
-    msg.set_content(f'Happy birthday {Name}!\nI hope all your birthday wishes and dreams come true.')
+    msg.set_content(f'Happy birthday {Name}!\n{Text}')
     
     urllib.request.urlretrieve('https://thumbs.dreamstime.com/b/colorful-happy-birthday-cupcakes-candles-spelling-148323072.jpg',"HBD.jpg")
     
     with open('HBD.jpg','rb') as f:
-        file_data = f.read()
-        file_type = imghdr.what(f.name)
-        file_name = f.name
-        
-        
-    msg.add_attachment(file_data,maintype='image',subtype=file_type,filename= file_name)
-    server.send_message(msg)
-    
-    server.quit()
-
-def sendWTPM(Name,Number):
-    img = 'B1.jpg'
-    body = f'Happy birthday {Name}!\nI hope all your birthday wishes and dreams come true.'
-    pywhatkit.sendwhats_image(Number,img,body,15,False,3)
-
-
-#FM: Festival Message
-def sendFM(Text,Reciver_Mail):
-    server = smtplib.SMTP('smtp.gmail.com',587) #Created gmail's server, and connected to gmail API
-    # Adding transfer layered security
-    server.starttls()
-    server.login(Sender_Mail,password) # Email, App password are inserted.
-
-        
-    msg['Subject'] = 'Testing of Festival Service!'
-    msg['From'] = Sender_Mail
-    msg['To'] = Reciver_Mail
-
-    
-    msg.set_content(f'{Text}')
-    
-    urllib.request.urlretrieve('https://images.pexels.com/photos/3149896/pexels-photo-3149896.jpeg',"MerryChristmas.jpeg")
-    
-    with open('MerryChristmas.jpeg','rb') as f:
         file_data = f.read()
         file_type = imghdr.what(f.name)
         file_name = f.name
@@ -76,8 +43,46 @@ def sendFM(Text,Reciver_Mail):
     del msg['To']
     msg.clear_content()
 
-def sendFMWT(Number,Photo,Text):
+def sendWTPM(Name,Number,Text):
+    img = 'HBD.jpg'
+    body = f'Happy birthday {Name}!\n{Text}.'
+    pywhatkit.sendwhats_image(Number,img,body,15,False,3)
+
+
+#FM: Festival Message
+def sendFM(Reciver_Mail,Text):
+    server = smtplib.SMTP('smtp.gmail.com',587) #Created gmail's server, and connected to gmail API
+    # Adding transfer layered security
+    server.starttls()
+    server.login(Sender_Mail,password) # Email, App password are inserted.
+
+        
+    msg['Subject'] = 'Testing of Festival Service!'
+    msg['From'] = Sender_Mail
+    msg['To'] = Reciver_Mail
+
     
+    msg.set_content(f'{Text}')
+    
+    urllib.request.urlretrieve('https://static.toiimg.com/thumb/resizemode-4,width-1200,height-900,msid-79947294/79947294.jpg',"MerryChristmas.jpg")
+    
+    with open('MerryChristmas.jpg','rb') as f:
+        file_data = f.read()
+        file_type = imghdr.what(f.name)
+        file_name = f.name
+        
+        
+    msg.add_attachment(file_data,maintype='image',subtype=file_type,filename= file_name)
+    server.send_message(msg)
+    
+    server.quit()
+    del msg['From']
+    del msg['Subject']
+    del msg['To']
+    msg.clear_content()
+
+def sendFMWT(Number,Text):
+    Photo = 'MerryChristmas.jpg'
     body = f'{Text}'
     pywhatkit.sendwhats_image(Number,Photo,body,15,False,3)
     
